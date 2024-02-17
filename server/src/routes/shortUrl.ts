0bdd8b159d491677d16fced646fa3d979382ee05
Mohit Pardhi <mpardhi11@gmail.com>
2024-02-17 17:07:48 +0530

    added API routes

--- server/src/routes/shortUrl.ts
+++ server/src/routes/shortUrl.ts
@@ -1,10 +1,15 @@
 import express from "express";
-
+import {
+  createUrl,
+  deleteUrl,
+  getAllUrl,
+  getUrl,
+} from "../controller/shortUrl.ts";
 const router = express.Router();
 
-// router.post("/shortUrl", createUrl);
-// router.get("/shortUrl", getAllUrl);
-// router.get("/shortUrl/:id", getUrl);
-// router.delete("/shortUrl", deleteUrl);
+router.post("/shortUrl", createUrl);
+router.get("/shortUrl", getAllUrl);
+router.get("/shortUrl/:id", getUrl);
+router.delete("/shortUrl", deleteUrl);
 
 export default router;
